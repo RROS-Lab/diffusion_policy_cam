2af450270f7f5a5448a5bfa85c27b404cc7d4c57
rishabh shukla <33500470+rysabh@users.noreply.github.com>
2024-07-13 19:25:57 -0700

    removed unnecessary files

--- rishabh DoE programs/test.py
+++ rishabh DoE programs/test.py
@@ -3,4 +3,18 @@ from submodules import robomath_addon as rma
 
 import numpy as np
 
+
+#read csv file
+import csv
+
+file_path = './../datasets/Chisel_problem/30/cleaned_test_data/test_127_cleaned.csv'
+
+with open(file_path, 'r', newline='') as file:
+    reader = csv.reader(file)
+    headers = ['gripper_x', 'gripper_y', 'gripper_z', 'gripper_w', 'gripper_X',
+            'gripper_Y', 'gripper_Z', 'chisel_x', 'chisel_y', 'chisel_z', 'chisel_w', 'chisel_X', 'chisel_Y',
+            'chisel_Z']
+    
+
+
 PlotTraj3D().main(traj=np.random.rand(100, 7), density=10)
