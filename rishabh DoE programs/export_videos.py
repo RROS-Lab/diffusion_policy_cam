ab1dd88db37fabed0b265c484db4d8727bb457ca
rishabh shukla <33500470+rysabh@users.noreply.github.com>
2024-07-14 08:04:21 -0700

    ....

--- rishabh DoE programs/export_videos.py
+++ rishabh DoE programs/export_videos.py
@@ -2,8 +2,8 @@ from get_video_from_csv import get_video_from_csv
 import os
 from tqdm import tqdm  # Import tqdm for progress bar functionality
 
-SOURCE_DIR = './../datasets/jul15_prediction/temp'
-TARGET_DIR = './saved_animations'
+SOURCE_DIR = './../datasets/jul15_prediction/cleaned_test_data'
+TARGET_DIR = './saved_animations/cleaned_test_data'
 
 # Get all files in the source directory and filter out the csv files
 input_files = [f for f in os.listdir(SOURCE_DIR) if f.endswith('.csv')]
